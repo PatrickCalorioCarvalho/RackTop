5ab0da02e6573bd021bba4a76a212ddf081d53ee
Patrick.Carvalho <Patrick.Carvalho@SB032>
2026-03-03 09:56:41 -0300

    __CHECKSUM__

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,19 +1,18 @@
-$packageName = 'RackTop'
-$url = 'https://github.com/PatrickCalorioCarvalho/RackTop/releases/latest/download/RackTop-x86_64-pc-windows-gnu.exe'
+$ErrorActionPreference = 'Stop'
 
-$packageArgs = @{
-  packageName   = 'racktop'
-  fileType      = 'exe'
-  url           = 'https://github.com/PatrickCalorioCarvalho/RackTop/releases/download/v0.1.6/RackTop-windows.exe'
+$packageName    = 'racktop'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$exeName        = 'racktop.exe'
+$exePath        = Join-Path $toolsDir $exeName
 
-  checksum      = '4e7a63d3d461c9f3e25753b5343ce1c49c8d9b2e9c98d5a519aa465f2c75b885'
-  checksumType  = 'sha256'
+$url            = 'https://github.com/PatrickCalorioCarvalho/RackTop/releases/download/v0.1.8/RackTop-windows.exe'
 
-  silentArgs    = ''
-  validExitCodes= @(0)
-}
+$checksum       = '__CHECKSUM__'
+$checksumType   = 'sha256'
 
-Install-ChocolateyPackage `
-  -PackageName $packageName `
-  -FileType exe `
-  -Url $url
+Get-ChocolateyWebFile `
+  -PackageName    $packageName `
+  -FileFullPath   $exePath `
+  -Url            $url `
+  -Checksum       $checksum `
+  -ChecksumType   $checksumType
